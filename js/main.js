9b5651d944a703d657b8927df619bf427a53ea77
Alisson <alisson.mb@gmail.com>
2023-01-30 17:55:02 -0300

    configurado localStorage

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 const form = document.querySelector('#novoItem');
+const lista = document.querySelector('ul');
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach ((e) => {
+    criaElemento(e)
+})
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
+    const nome = e.target.elements['nome']
+    const quantidade = e.target.elements['quantidade']
+
+    const itemAtual = {
+        "quantidade": quantidade.value,
+        "nome": nome.value
+    } 
     
-    criaElemento(e.target.elements['nome'].value, e.target.elements['quantidade'].value)
+    criaElemento(itemAtual)
+    itens.push(itemAtual)
+    localStorage.setItem("itens", JSON.stringify(itens))
+    
+    quantidade.value = ""
+    nome.value = ""
+
 })
 
-function criaElemento(nome, qtde) {
+
+function criaElemento(item) {
     //incluir dados no DOM <li class="item"><strong>7</strong>Camisas</li>
-    console.log(nome)
-    console.log(qtde)
     //criando li (item da lista)
     const novoItem = document.createElement('li')
     //incluindo classe para o li
@@ -17,11 +35,13 @@ function criaElemento(nome, qtde) {
     //criando elemento strong
     const nrItem = document.createElement('strong')
     //incluindo a quantidade dentro no strong
-    nrItem.innerHTML = qtde
+    nrItem.innerHTML = item.quantidade
     //incluir o elemento strong no elemento lista
     novoItem.appendChild(nrItem)
     //incluindo quantidade no item
-    novoItem.innerHTML += nome
-    const lista = document.querySelector('ul');
+    novoItem.innerHTML += item.nome
     lista.appendChild(novoItem)
+
+
+
 } 
